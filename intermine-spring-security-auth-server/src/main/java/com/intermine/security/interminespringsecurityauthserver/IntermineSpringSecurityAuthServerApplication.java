a782a2cf8739c739e4904f7f8457409c243d321e
Rahul <ry9910725@gmail.com>
2019-03-24 03:45:58 +0530

    Resource configurations added

---
+++ intermine-spring-security-auth-server/src/main/java/com/intermine/security/interminespringsecurityauthserver/IntermineSpringSecurityAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.intermine.security.interminespringsecurityauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntermineSpringSecurityAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntermineSpringSecurityAuthServerApplication.class, args);
+	}
+
+}
