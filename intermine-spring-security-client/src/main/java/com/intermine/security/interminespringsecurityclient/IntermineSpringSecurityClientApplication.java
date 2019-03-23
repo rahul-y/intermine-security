cb303c1755e9f6721e6bf979aa712f1c692f7c5d
Rahul <ry9910725@gmail.com>
2019-03-24 05:17:58 +0530

    Client Configurations added

---
+++ intermine-spring-security-client/src/main/java/com/intermine/security/interminespringsecurityclient/IntermineSpringSecurityClientApplication.java
@@ -0,0 +1,13 @@
+package com.intermine.security.interminespringsecurityclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntermineSpringSecurityClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntermineSpringSecurityClientApplication.class, args);
+	}
+
+}
